48516def37889a6972a55bd716b89e49d391a234
seongkyulim <96x60812@gmail.com>
2025-09-16 15:41:38 +0900

    Chore: Spring boot project setting.

---
+++ src/main/kotlin/com/example/oauthloginpractice/OauthLoginPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.oauthloginpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthLoginPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<OauthLoginPracticeApplication>(*args)
+}
